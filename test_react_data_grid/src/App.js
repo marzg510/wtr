cb02ea26731fdd89941186dcc26094e9cf1c4605
marzg <masaru.gotou@gmail.com>
2023-02-17 03:56:53 +0000

    react-data-gridを試した

---
+++ test_react_data_grid/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import 'react-data-grid/lib/styles.css';
+import DataGrid from 'react-data-grid';
+
+const columns = [
+  { key: 'id', name: 'ID' },
+  { key: 'title', name: 'Title' }
+];
+
+const rows = [
+  { id: 0, title: 'Example' },
+  { id: 1, title: 'Demo' }
+];
+
+function App() {
+  return <DataGrid columns={columns} rows={rows} />;
+}
+
+export default App;
