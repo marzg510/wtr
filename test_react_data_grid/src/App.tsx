9b025f62d3ed16d55d2032b185ad53607b7d0e77
Masaru Gotou <masaru.gotou@gmail.com>
2023-02-20 23:56:49 +0900

    モックを少し作った

--- test_react_data_grid/src/App.tsx
+++ test_react_data_grid/src/App.tsx
@@ -7,15 +7,18 @@ import dateEditor, { timeEditor } from './DateEditor';
 
 const columns = [
   { key: 'id', name: 'ID', width: 10 },
-  { key: 'title', name: 'Title', width: 150, editor: textEditor },
-  { key: 'entryDate', name: 'EntryDate', width: 120, editor: dateEditor },
-  { key: 'startTime', name: 'StartDate', width: 120, editor: timeEditor },
+  { key: 'date', name: 'Date', width: 120, editor: dateEditor },
+  { key: 'startTime', name: 'Start', width: 80, editor: timeEditor },
+  { key: 'endTime', name: 'End', width: 80, editor: timeEditor },
+  { key: 'restTime', name: 'Rest', width: 80, editor: timeEditor },
+  { key: 'workTime', name: 'Working', width: 80 },
+  { key: 'work', name: 'Work', width: 300, editor: textEditor },
 ];
 
 function App() {
   const [rows,setRows] = useState([
-    { id: 0, title: 'Example', entryDate: '2022-01-01', startTime: '09:00' },
-    { id: 1, title: 'Demo' , entryDate: '2022-02-01', startTime: '10:00'},
+    { id: 0, date: '2022-01-01', startTime: '09:00', endTime: '10:00', restTime:'0:00' },
+    { id: 1, date: '2022-02-01', startTime: '10:00', endTime: '11:00', restTime:'0:00' },
   ]);
   return (
     <DataGrid
