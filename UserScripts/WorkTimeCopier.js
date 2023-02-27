ff35af18c6c4ee99b01fe4f33ed0ac534ec0bcd3
masaru gotou <masaru.gotou@scsk.jp>
2023-02-27 21:04:42 +0900

    リファクタリング

--- UserScripts/WorkTimeCopier.js
+++ UserScripts/WorkTimeCopier.js
@@ -30,9 +30,6 @@
     $(btn).click()
     Cookies.set(cookieKeyCopyCount, --cookieCopyCount)
   }
-  // var value=localStorage.getItem("m510.key1");
-  // $(td).after('<td><div id="test-m510"></div></td>');
-  // $('#test-m510').append('<input type="BUTTON" name="testgoto" id="btn-test-m510" value="テスト2" />');
   // 貼り付け用テキストボックスを追加
   addTextArea();
   // ５行コピーボタンを追加
@@ -56,20 +53,6 @@
     }
     // フォームのクリア処理
     clearForm();
-      // var tbl = document.getElementsByTagName("table");
-      // console.log("tbl", typeof(tbl));
-      // console.log("tbl", tbl);
-      // Array.prototype.forEach.call(tbl,function(t) {
-        // if ( t.getAttribute("summary") == "管理単位" ) {
-          // var tr = t.getElementsByTagName("tr")[2];
-          // console.log("tr2",tr);
-          // var td = tr.getElementsByTagName("td")[2];
-          // console.log("td2",td);
-          // var inp = td.getElementsByTagName("input")[0];
-          // console.log("inp",inp);
-          // inp.value = "タスク";
-        // }
-      // });
     lines.some((line,index) => {
       if ( line.trim().length === 0 ) return;
       var fields = line.split('\t');
@@ -115,23 +98,6 @@
   // フォームのクリア
   function clearForm() {
     console.log("clear form");
-    // var tbl = document.getElementsByTagName("table");
-    // Array.prototype.forEach.call(tbl,function(t) {
-    //   if ( t.getAttribute("summary") == "管理単位" ) {
-    //     var inps = t.getElementsByTagName("input");
-    //     Array.prototype.forEach.call(inps,function(inp) {
-    //       // if ( inp.getAttribute("type") != "button") {
-    //       if ( inp.getAttribute("name") == "task" ||
-    //            inp.getAttribute("name") == "minsH" ||
-    //            inp.getAttribute("name") == "minsM") {
-    //         console.log("inp",inp);
-    //         inp.value = "";
-    //         inp.dispatchEvent(new Event('change'));
-    //         inp.dispatchEvent(new Event('blur'));
-    //       }
-    //     });
-    //   }
-    // });
     $('table[summary="管理単位"] input').not('[type="button"]').not('[type="hidden"]').each(function(index) {
       $(this).val("").change().blur();
     });
@@ -149,8 +115,6 @@
       var emptyRow = $('input[name="minsH"]', rows).filter(function() {
         return $.trim($(this).val()) === '';
       }).parent().parent().first();
-      // var emptyTaskTd = $('td:has(input[name="task"])',emptyRow)
-      // var emptyTimeTd = $('td:has(input[name="minsH"])',emptyRow)
       console.log( "project row", rows );
       console.log( "empty row", emptyRow );
       // console.log( "empty row time td val", $('input[name="minsH"]',emptyTimeTd).val() );
